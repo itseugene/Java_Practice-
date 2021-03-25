2d456c2d3b79210603b14f7906c3ab6bb0c86e7f
Eugene Lee <elee05@mica.edu>
2021-03-25 18:47:30 +0900

    1

---
+++ Java_git/src/Pack_git/World.java
@@ -0,0 +1,10 @@
+package Pack_git;
+
+public class World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//https://github.com/itseugene/Java_Practice-.git
+	}
+
+}
